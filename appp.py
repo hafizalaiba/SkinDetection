ea200eaee6afd766a654d0942f60a4f923387f26
hafizalaiba <hafizalaiba321@gmail.com>
2025-08-21 03:46:23 +0500

    Update appp.py

--- appp.py
+++ appp.py
@@ -408,4 +408,5 @@ def logout():
     return redirect(url_for('home'))  # Redirect to homepage or login page
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000)
+
